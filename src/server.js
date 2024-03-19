321077527b1a40298a9dcfcfb017fdfa509613cc
John Doe <betiaman@example.com>
2024-03-19 11:07:26 -0500

    basic set up of authentication using passport

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const { PORT = 8000 } = process.env;
 const app = require("./app");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/passport-auth", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 app.listen(PORT, listener);
