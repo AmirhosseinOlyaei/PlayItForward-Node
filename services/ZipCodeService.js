846a2da5da505907db8591e2789647c99a979e45
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-14 22:03:55 -0400

    "Update ZipCodeService and geoController to return empty array when no location found"
    This commit updates the ZipCodeService and geoController to return an empty array instead of throwing an error when no location is found

--- services/ZipCodeService.js
+++ services/ZipCodeService.js
@@ -3,18 +3,11 @@ const ZipCode = require("../models/ZipCode");
 
 class ZipCodeService {
   static async findZipCodesByPlaceId(placeId) {
-    try {
-      const location = await ZipCode.findOne({ placeId: placeId });
-      if (!location) {
-        return []; // Return an empty array if no location found
-      }
-      return location.zipCodes;
-    } catch (error) {
-      console.error("Database error when fetching zip codes:", error);
-      throw new Error(
-        "Database error when fetching zip codes: " + error.message
-      );
+    const location = await ZipCode.findOne({ placeId: placeId });
+    if (!location) {
+      return []; // Return an empty array if no location found
     }
+    return location.zipCodes;
   }
 }
 
--- src/controller/geoController.js
+++ src/controller/geoController.js
@@ -3,18 +3,11 @@ const ZipCodeService = require("../../services/ZipCodeService");
 
 exports.getZipCodesByPlaceId = async (req, res) => {
   const { placeId } = req.query;
-  try {
-    const zipCodes = await ZipCodeService.findZipCodesByPlaceId(placeId);
-    if (zipCodes.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "No zip codes found for the provided place ID" });
-    }
-    res.json({ zipCodes });
-  } catch (error) {
-    console.error("Failed to fetch zip codes:", error);
-    res
-      .status(500)
-      .json({ message: "Failed to fetch zip codes", error: error.message });
+  const zipCodes = await ZipCodeService.findZipCodesByPlaceId(placeId);
+  if (zipCodes.length === 0) {
+    return res
+      .status(200)
+      .json({ message: "No zip codes found for the provided place ID" });
   }
+  res.json({ zipCodes });
 };
