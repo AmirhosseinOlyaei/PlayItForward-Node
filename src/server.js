66ef15e7fc98de1a0c0eff3718bd33ef777c06ad
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-23 02:31:18 -0400

    Change process.env.PORT assignment to use logical OR operator

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-const { PORT = 8000 } = process.env;
+// src/server.js
+// const { PORT = 8000 } = process.env;
+const PORT = process.env.PORT || 8000;
 const app = require("./app");
 const passport = require("passport");
 const listener = () => console.log(`Listening on Port ${PORT}!`);
