7925c1350b2ce3b52059e4845b1af40cf029631d
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-04-23 03:04:39 -0400

    Add error handling middleware
    - Add 404 not found middleware
    - Add error handler middleware

--- src/app.js
+++ src/app.js
@@ -88,3 +88,21 @@ connectDB();
 
 // Server
 module.exports = app;
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
