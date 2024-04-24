0dd9c9e777c6537cb0999ff14df87bae2c3d63df
gsiryvhb <gaetansiry@gmail.com>
2024-04-24 17:50:21 -0400

    fix toy id param

--- src/router/toyListingRouter.js
+++ src/router/toyListingRouter.js
@@ -5,9 +5,9 @@ const toyListingController = require("../controller/toyListingController.js");
 // Define routes
 router.post("/", toyListingController.createToyListing); // POST new toy listing
 router.get("/", toyListingController.getAllToyListings); // GET all toy listings
-router.get("/:toyListingid", toyListingController.getToyListing); // GET toy listing by ID
-router.put("/:toyListingId", toyListingController.updateToyListing); //update toy listing
-router.delete("/:toyListingId", toyListingController.deleteToyListing); //delete toy listing
+router.get("/:id", toyListingController.getToyListing); // GET toy listing by ID
+router.put("/:id", toyListingController.updateToyListing); //update toy listing
+router.delete("/:id", toyListingController.deleteToyListing); //delete toy listing
 router.get("/enums/:fieldName", toyListingController.getEnumValues); //get enum values
 
 router.get("/user/:userId", toyListingController.getToyListingsByUser); // GET toy listings by user ID
