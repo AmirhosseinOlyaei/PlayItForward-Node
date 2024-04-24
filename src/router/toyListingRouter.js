314a0e365fc98d3981bd6b96f4c7c7f84a59f5c3
John Doe <betiaman@example.com>
2024-04-24 13:33:03 -0500

    adds route to fetch toy listing by user id

--- src/router/toyListingRouter.js
+++ src/router/toyListingRouter.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const toyListingController = require("../controller/toyListingController.js");
 
 // Define routes
-router.post("/", toyListingController.createToyListing);
-router.get("/", toyListingController.getAllToyListings);
-router.get("/:id", toyListingController.getToyListing);
-router.get("/enums/:fieldName", toyListingController.getEnumValues);
-router.put("/:toyListingId", toyListingController.updateToyListing);
-router.delete("/:toyListingId", toyListingController.deleteToyListing);
+router.post("/", toyListingController.createToyListing); // POST new toy listing
+router.get("/", toyListingController.getAllToyListings); // GET all toy listings
+router.get("/:toyListingid", toyListingController.getToyListing); // GET toy listing by ID
+router.put("/:toyListingId", toyListingController.updateToyListing); //update toy listing
+router.delete("/:toyListingId", toyListingController.deleteToyListing); //delete toy listing
+router.get("/enums/:fieldName", toyListingController.getEnumValues); //get enum values
+
+router.get("/user/:userId", toyListingController.getToyListingsByUserId); // GET toy listings by user ID
 
 module.exports = router;
