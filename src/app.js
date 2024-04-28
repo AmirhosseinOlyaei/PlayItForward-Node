56916ca2824a9c187ed8ddf26efaaa48f1dd6a31
gsiryvhb <gaetansiry@gmail.com>
2024-04-28 16:10:13 -0400

    Update app.js
    added more origins

--- src/app.js
+++ src/app.js
@@ -34,7 +34,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+// app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+
+const allowedOrigins = [process.env.FRONTEND_URL, 'http:/localhost:5173'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1){
+            var msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
+}));
+
 app.set("trust proxy", 1);
 // Initialize Passport and sessions for Passport
 app.use(passport.initialize());
