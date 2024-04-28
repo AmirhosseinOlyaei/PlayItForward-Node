63b4f1eadd67b7043ca62330b6382d900c0439e8
John Doe <betiaman@example.com>
2024-04-28 12:43:57 -0500

    integrates router with application

--- src/app.js
+++ src/app.js
@@ -49,6 +49,7 @@ const messageRouter = require("./router/messageRouter.js");
 const favoriteToyRouter = require("./router/favoriteToyRouter.js");
 const searchRouter = require("./router/searchRouter.js");
 const imageRouter = require("./router/imageRouter.js");
+const termsAgreedRouter = require("./router/termsAgreementRouter.js");
 
 // Using routers
 app.use("/api/v1", mainRouter);
@@ -61,6 +62,7 @@ app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/favorites", favoriteToyRouter);
 app.use("/api/v1/search", searchRouter);
 app.use("/api/v1/images", imageRouter);
+app.use("/api/v1/terms", termsAgreedRouter);
 
 app.use("/api/v1/user", (req, res) => {
   if (req.isAuthenticated()) {
