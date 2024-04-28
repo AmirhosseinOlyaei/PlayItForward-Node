1992098f6a8fd4696d5e08a504b1e854725b4e89
John Doe <betiaman@example.com>
2024-04-28 13:29:43 -0500

    updates terms router and controller - test in postman passed

--- src/controller/termsAgreementController.js
+++ src/controller/termsAgreementController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user"); // Adjust the path as needed
+const User = require("../../models/user"); // Adjust the path as needed
 
 // Function to record agreement to the terms
 exports.agreeToTerms = async (req, res) => {
@@ -7,18 +7,16 @@ exports.agreeToTerms = async (req, res) => {
   try {
     const updatedUser = await User.findByIdAndUpdate(
       userId,
-      { agreedToTerms: true },
-      { new: true }
+      { termsAndConditions: true }, // Ensure we're updating this specific field
+      { new: true, runValidators: true }
     );
     if (!updatedUser) {
       return res.status(404).json({ message: "User not found" });
     }
-    res
-      .status(200)
-      .json({
-        message: "Terms agreement updated successfully",
-        user: updatedUser,
-      });
+    res.status(200).json({
+      message: "Terms agreement updated successfully",
+      user: updatedUser,
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -29,13 +27,13 @@ exports.checkTermsAgreement = async (req, res) => {
   const { userId } = req.params;
 
   try {
-    const user = await User.findById(userId);
+    const user = await User.findById(userId, "termsAndConditions"); // Fetching only the termsAndConditions field
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
     res
       .status(200)
-      .json({ userId: user._id, agreedToTerms: user.agreedToTerms });
+      .json({ userId: user._id, termsAndConditions: user.termsAndConditions });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- src/router/termsAgreementRouter.js
+++ src/router/termsAgreementRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const termsController = require("./termsController"); // Adjust the path as needed
+const termsController = require("../controller/termsAgreementController"); // Adjust the path as needed
 
 router.put("/agree/:userId", termsController.agreeToTerms);
 router.get("/check/:userId", termsController.checkTermsAgreement);
