2ec5fafe5f429b08f6aeec5a880c84c70bf1d5a8
Amirhossein Olyaei <amirholyaei@gmail.com>
2024-06-27 02:26:34 -0400

    "Updated authRouter.js to add middleware, routes for signup, signin, forgot password, reset password, and logout, and modified Google authentication routes"

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -1,30 +1,49 @@
 // src/router/authRouter.js
-const express = require("express");
+require("dotenv").config();
+const router = require("express").Router();
 const passport = require("passport");
 const authController = require("../controller/authController.js");
-const router = express.Router();
 
-// Local authentication routes
+// Middleware to ensure that a user is authenticated
+function checkIfUserIsAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).send("User is not authenticated!");
+}
+
+// Route to handle user signup
 router.post("/signup", authController.signup);
+
+// Route to handle user signin
 router.post("/signin", authController.signin);
+
+// Route to handle forgot password
 router.post("/forgot-password", authController.forgotPassword);
 
-// Google authentication routes
+// Route to handle reset password
+router.post("/reset-password/:token", authController.resetPassword);
+
+// Route to initiate the authentication process with Google
 router.get("/google", authController.authenticateGoogle);
+
+// Route to handle Google authentication callback
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
   function (req, res) {
+    // Successful authentication, gets redirected.
     res.redirect(`${process.env.FRONTEND_URL}`);
   }
 );
 
+// Route to handle user logout
 router.get("/logout", (req, res) => {
   req.logout(function (err) {
     if (err) {
       return next(err);
     }
-    res.clearCookie("connect.sid", { path: "/" });
+    res.clearCookie("connect.sid", { path: "/" }); // Clear session cookie
     res.redirect(process.env.FRONTEND_URL);
   });
 });
