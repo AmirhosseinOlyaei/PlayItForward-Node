4bc913559765ffff5cd7e00d6c65c73821ff6559
Amirhossein Olyaei <amirholyaei@gmail.com>
2024-06-27 01:34:44 -0400

    Refactor authentication strategies and add Google OAuth2 support
    Integrate Google OAuth2 authentication strategy, enhancing user login capabilities, and enabling new users to authenticate via Google. This change also improves the overall user experience and expands authentication options.

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,8 @@
 // services/passport.js
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const User = require("./models/User"); // this would be the user model
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/user");
 
 passport.use(
   new LocalStrategy(
@@ -9,10 +10,14 @@ passport.use(
       usernameField: "email",
       passwordField: "password",
     },
-    async (email, passoword, done) => {
+    async (email, password, done) => {
       try {
         const user = await User.findOne({ email });
-        if (!user || !user.validPassword(password)) {
+        if (!user) {
+          return done(null, false, { message: "Incorrect email or password" });
+        }
+        const isValid = await user.validPassword(password);
+        if (!isValid) {
           return done(null, false, { message: "Incorrect email or password" });
         }
         return done(null, user);
@@ -22,6 +27,46 @@ passport.use(
     }
   )
 );
+
+passport.use(
+  new GoogleStrategy(
+    {
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const currentUser = await User.findOne({ googleId: profile.id });
+        if (currentUser) {
+          currentUser.accessToken = accessToken;
+          currentUser.refreshToken = refreshToken;
+          await currentUser.save();
+          done(null, currentUser);
+        } else {
+          const newUser = new User({
+            googleId: profile.id,
+            email: profile.emails[0].value,
+            first_name: profile.name.givenName,
+            last_name: profile.name.familyName,
+            nickname: profile.displayName,
+            profile_picture: profile._json.picture,
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+          });
+          await newUser.save();
+          done(null, newUser);
+        }
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
 passport.deserializeUser(async (id, done) => {
   try {
