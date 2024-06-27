5de7d698906ed0c6a9d22333361f3e70c4a00538
Amirhossein Olyaei <amirholyaei@gmail.com>
2024-06-27 01:38:31 -0400

    Refactor authentication functions and implement forgot password functionality
    Added JWT generation function, bcrypt, nodemailer usage for password reset,
    and email notifications on password reset request. Improves user experience.

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,10 +1,91 @@
 // src/controller/authController.js
+const User = require("../../models/user");
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+
+// Helper function to generate JWT
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+};
+
+exports.signup = async (req, res) => {
+  try {
+    const { email, password, first_name, last_name } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    const user = new User({ email, password, first_name, last_name });
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.signin = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      const token = generateToken(user);
+      res.json({ token, user });
+    });
+  })(req, res, next);
+};
+
+exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      to: user.email,
+      from: process.env.GMAIL_USER,
+      subject: "Password Reset",
+      text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+             Please click on the following link, or paste this into your browser to complete the process:\n\n
+             http://${req.headers.host}/reset-password/${token}\n\n
+             If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.json({ message: "Password reset link sent" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 exports.authenticateGoogle = passport.authenticate("google", {
   scope: ["profile", "email"],
 });
 
 exports.googleAuthCallback = (req, res) => {
-  res.send("you have been redirected to the homepage");
+  res.send("You have been redirected to the homepage");
 };
